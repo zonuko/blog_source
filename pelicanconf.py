342cdb8e20b44812a79b021e1e88b100312d221e
y-fujiwara <fuji.ys5234@gmail.com>
2017-04-09 21:34:28 +0900

    change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,25 +27,34 @@ DATE_FORMATS = {
 }
 
 DEFAULT_LANG = 'ja'
-THEME = "../themes/pelican-mg"
+THEME = "../themes/pelican-bootstrap3"
 PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['assets', 'better_codeblock_line_numbering', 'render_math', 'related_posts', 'tag_cloud', 'tipue_search', 'sitemap']
-JINJA_EXTENSIONS = ['webassets.ext.jinja2.AssetsExtension', 'jinja2.ext.with_']
+PLUGINS = ['i18n_subsites', 'pelican_youtube', 'assets', 'better_codeblock_line_numbering', 'render_math', 'related_posts', 'tag_cloud', 'tipue_search', 'sitemap']
+
+# PelicanのバージョンアップによってJinjaの拡張の書き方は以下のように変わったようだ
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n', 'webassets.ext.jinja2.AssetsExtension', 'jinja2.ext.with_']
+}
+
+# pelican-bootstrap3関係の設定
+BOOTSTRAP_THEME = "simplex"
+PYGMENTS_STYLE = "solarizeddark"
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+TWITTER_CARDS = True
+# ADDTHIS_FACEBOOK_LIKE  = True
+# ADDTHIS_TWEET          = True
+ADDTHIS_GOOGLE_PLUSONE = False
+ADDTHIS_DATA_TRACK_ADDRESSBAR = True
+ADDTHIS_PROFILE  = "ra-58ea037a1776945e"
+# BOOTSTRAP_FLUID = True
 
 SITEMAP = {
     'format': 'xml'
 }
 
-# Plugins setting
 RELATED_POSTS_MAX = 10
 RELATED_POSTS_SKIP_SAME_CATEGORY = True
-
-# Feed generation is usually not desired when developing
-# FEED_ALL_ATOM = None
-# CATEGORY_FEED_ATOM = None
-# TRANSLATION_FEED_ATOM = None
-# AUTHOR_FEED_ATOM = None
-# AUTHOR_FEED_RSS = None
 
 # Social widget
 SOCIAL = (
@@ -62,14 +71,24 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-TAG_SAVE_AS = ''
-AUTHOR_SAVE_AS = ''
-DIRECT_TEMPLATES = ('index', 'categories', 'archives', 'search', 'tipue_search')
+DIRECT_TEMPLATES = ('index', 'categories', 'archives', 'search') #, 'tipue_search')
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-SHARE = True
-TWITTER_USERNAME = 'nuhera'
+
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
 
 # 色々パスの設定
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+
+MENUITEMS = (
+    ('HOME', '/'),
+    ('ATOM', '/feeds/all.atom.xml')
+)
